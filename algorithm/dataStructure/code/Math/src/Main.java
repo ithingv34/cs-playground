2a6b4537a232d875dbf2879e6ede142be3c09b7a
ithingv34 <ithingv34@gmail.com>
2022-12-18 18:57:57 +0900

    경우의수 코드 업로드

--- algorithm/dataStructure/code/Math/src/Main.java
+++ algorithm/dataStructure/code/Math/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -7,7 +8,7 @@ public class Main {
         // 자바에서 HashSet 사용
         // .add()
         // .remove(idx)
-        // .size()
+        // .size()g
         // .contains(n)
 
         HashSet a = new HashSet<>(Arrays.asList(1,2,3,4,5));
@@ -35,7 +36,6 @@ public class Main {
         HashSet h = new HashSet(Arrays.asList(1,2));
         System.out.println(g.containsAll(h));
     }
-
     static void printMySet() {
 
         System.out.println();
@@ -55,10 +55,72 @@ public class Main {
         result = a.removeAll(b);
         System.out.println("차집합:" + result.list);
     }
+    static void printNumberOfSet(){
+        // 두개의 주사위를 던졌을 때 합이 3 또는 4의 배수일 경우의 수
+        int[] A = {1, 2, 3, 4, 5, 6};
+        int[] B = {1, 2, 3, 4, 5, 6};
+
+        int nA = 0;
+        int nB = 0;
+        int nAandB = 0;
+
+        // 기본 풀이
+        for (int itemA: A) {
+            for (int itemB: B) {
+                if ((itemA + itemB) % 3 == 0) {
+                    // 3의 배수
+                    nA += 1;
+                }
+                if ((itemA + itemB) % 4 == 0) {
+                    // 4의 배수
+                    nB += 1;
+                }
+                if ((itemA + itemB) % 12 == 0) {
+                    nAandB += 1;
+                }
+            }
+        }
+        System.out.println("결과: " + (nA + nB - nAandB));
+
+        // HashSet 이용
+        HashSet<ArrayList> allCases = new HashSet<>();
+        for (int itemA : A) {
+            for (int itemB: B) {
+                if ((itemA + itemB) % 3 == 0 || (itemA + itemB) % 4 == 0) {
+                    ArrayList list = new ArrayList(Arrays.asList(itemA, itemB));
+                    allCases.add(list);
+                }
+            }
+        }
+        System.out.println("결과: " + allCases.size());
+
+        // 곱의 법칙
+
+        nA = 0;
+        nB = 0;
+
+        for (int itemA : A) {
+            if (itemA % 3 == 0) {
+                nA++;
+            }
+        }
+
+        for (int itemB: B) {
+            if (itemB % 4 == 0) {
+                nB++;
+            }
+        }
+        System.out.println("결과: " + (nA * nB));
+    }
 
     public static void main(String[] args) {
 
-        printHashSet();
-        printMySet();
+        // 1_Set
+//        printHashSet();
+//        printMySet();
+
+        // 2_Number Of Set
+        printNumberOfSet();
+
     }
 }
