782207832f73a5aa59836ab3cc4e7f41125696c2
ithingv34 <ithingv34@gmail.com>
2022-12-24 22:16:25 +0900

    datastructure

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -67,12 +67,137 @@ module.exports = {
       '/datastructure/': [
         {
           title: '자료구조',
-          collapsable: false,
+          collapsable: true,
           children: [
             '',
-            'using-vue',
           ]
-        }
+        },
+        {
+          title: '수학',
+          collapsable: true,
+          children: [
+            {
+              title: '집합',
+              collapsable: true,
+              children: [
+                '/datastructure/Math/1_Set/',
+              ]
+            },
+            {
+              title: '경우의수',
+              collapsable: true,
+              children: [
+                '/datastructure/Math/2_Number_of_cases/',
+              ]
+            },
+            {
+              title: '순열',
+              collapsable: true,
+              children: [
+                '/datastructure/Math/3_Permutations/',
+              ]
+            },
+            {
+              title: '조합',
+              collapsable: true,
+              children: [
+                '/datastructure/Math/4_Combination/',
+              ]
+            },
+            {
+              title: '지수와 로그',
+              collapsable: true,
+              children: [
+                '/datastructure/Math/5_Exponential_log/',
+              ]
+            },
+            {
+              title: '재귀',
+              collapsable: true,
+              children: [
+                '/datastructure/Math/6_Recursion/',
+              ]
+            },
+            {
+              title: '시간복잡도',
+              collapsable: true,
+              children: [
+                '/datastructure/Math/7_Time_Complexity/',
+              ]
+            }
+
+          ]
+        },
+        {
+          title: '배열',
+          collapsable: true,
+          children: [
+            '/datastructure/Array/'
+          ]
+        },
+        {
+          title: '연결리스트',
+          collapsable: true,
+          children: [
+            '/datastructure/LinkedList/'
+          ]
+        },
+        {
+          title: '스택',
+          collapsable: true,
+          children: [
+            '/datastructure/Stack/'
+          ]
+        },
+        {
+          title: '큐',
+          collapsable: true,
+          children: [
+            '/datastructure/Queue/'
+          ]
+        },
+        {
+          title: '덱',
+          collapsable: true,
+          children: [
+            '/datastructure/Deck/'
+          ]
+        },
+        {
+          title: '해시테이블',
+          collapsable: true,
+          children: [
+            '/datastructure/HashTable/'
+          ]
+        },
+        {
+          title: '트리',
+          collapsable: true,
+          children: [
+            '/datastructure/Tree/'
+          ]
+        },
+        {
+          title: '힙',
+          collapsable: true,
+          children: [
+            '/datastructure/Heap/'
+          ]
+        },
+        {
+          title: '그래프',
+          collapsable: true,
+          children: [
+            '/datastructure/Graph/'
+          ]
+        },
+        {
+          title: '트라이',
+          collapsable: true,
+          children: [
+            '/datastructure/Trie/'
+          ]
+        },
       ],
     },
   },
--- docs/src/datastructure/README.md
+++ docs/src/datastructure/README.md
@@ -1,3 +1,38 @@
-# 자료구조
+# 소개 및 목차
 
-<img src="./../../images/datastructure/1-1.png" style="margin: 30px 100px">
+Java를 활용하여 자료구조를 학습한 내용을 정리한 문서입니다.
+
+
+<br>
+
+**목차**
+
+---
+
+- [수학](./Math/README.md)
+  - [집합](./Math/1_Set.md)
+  - [경우의수](./Math/2_Number_of_cases.md)
+  - [순열](./Math/3_Permutations.md)
+  - [조합](./Math/4_Combinations.md)
+  - [지수/로그](./Math/5_Exponential_Log.md)
+  - [재귀](./Math/6_Recursion.md)
+  - [시간복잡도](./Math/7_Time_Complexity.md)
+
+----
+
+**선형 자료구조**
+
+- [배열](./Array/README.md)
+- [연결리스트](./LinkedList/README)
+- [스택](./Stack/README)
+- [큐](./Queue/README)
+- [덱](./Deck/README)
+- [해시테이블](./HashTable/README)
+
+---
+**비선형 자료구조**
+- [트리](./Tree/README)
+- [힙](./Heap/README)
+- [그래프](./Graph/README)
+- [트라이](./Tree/README)
+
